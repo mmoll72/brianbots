f34e8b12bfeddec3c762fd1d781e2c194672fb30
bmbowman2000 <bmbowman@uvm.edu>
2024-01-22 22:53:27 -0500

    opened simulation window

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time
+
+SLEEP_TIME = 1 / 120
 
 physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+        p.stepSimulation()
+        time.sleep(SLEEP_TIME)
+        print(i)
+
 p.disconnect()
